a79189cc31d9e3766d589c987a5700741e78df2e
杨博 <pop.atry@gmail.com>
2012-03-26 17:02:27 +0800

    Update project/project/PluginBuild.scala

--- project/project/PluginBuild.scala
+++ project/project/PluginBuild.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 杨博 (Yang Bo)
+ * Copyright 2012 杨博 (Yang Bo)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
